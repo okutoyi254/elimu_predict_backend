5574d451af82fe92c52499b1fb36e4b996ddce3a
James Okutoyi <okutoyijemo@gmail.com>
2026-03-07 17:11:29 +0300

    Initial springboot project setup

---
+++ src/main/java/com/elimupredict/ElimuPredict/ElimuPredictApplication.java
@@ -0,0 +1,13 @@
+package com.elimupredict.ElimuPredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElimuPredictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElimuPredictApplication.class, args);
+	}
+
+}
