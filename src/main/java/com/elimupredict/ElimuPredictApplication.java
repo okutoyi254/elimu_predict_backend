dc651ceaa954db482d33a3e55164c01e16466208
James Okutoyi <okutoyijemo@gmail.com>
2026-03-24 12:17:43 +0300

    Configure virtual threads for lightweight processing

--- src/main/java/com/elimupredict/ElimuPredictApplication.java
+++ src/main/java/com/elimupredict/ElimuPredictApplication.java
@@ -1,7 +1,10 @@
 package com.elimupredict;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ElimuPredictApplication {
@@ -10,4 +13,14 @@ public class ElimuPredictApplication {
 		SpringApplication.run(ElimuPredictApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner verifyVirtualThreads() {
+		return args -> {
+			Thread.ofVirtual().start(() ->
+					LoggerFactory.getLogger(ElimuPredictApplication.class)
+							.info("Virtual threads active: {}",
+									Thread.currentThread().isVirtual())
+			);
+		};
+	}
 }
--- src/main/java/com/elimupredict/common/AppConfig.java
+++ src/main/java/com/elimupredict/common/AppConfig.java
@@ -2,13 +2,20 @@ package com.elimupredict.common;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory =
+                new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(10000);
+        factory.setReadTimeout(30000);
+        
+        return new RestTemplate(factory);
     }
 }
